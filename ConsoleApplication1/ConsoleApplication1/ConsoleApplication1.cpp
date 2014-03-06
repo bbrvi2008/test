6d278c202d33d87b2f968c60b8aea95bc3358e7b
bbrvi2008 <bbrvi1993@gmail.com>
2014-03-06 15:43:42 +0800

    commit2

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,10 @@
-// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
-
+#include <iostream>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::cout << "Hello world!" << std::endl;
+
 	return 0;
 }
 
